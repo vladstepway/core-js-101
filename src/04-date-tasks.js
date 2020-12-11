700053587b33ba4ff8d4121da8fec0a2d237a8dc
ustsepavy <vladstepovoyvios@gmail.com>
2020-12-11 17:30:57 +0300

    feat: added date task

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -19,8 +19,8 @@
  *    'Tue, 26 Jan 2016 13:48:02 GMT' => Date()
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
-function parseDataFromRfc2822(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromRfc2822(value) {
+  return new Date(value);
 }
 
 /**
@@ -34,8 +34,8 @@ function parseDataFromRfc2822(/* value */) {
  *    '2016-01-19T16:07:37+00:00'    => Date()
  *    '2016-01-19T08:07:37Z' => Date()
  */
-function parseDataFromIso8601(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromIso8601(value) {
+  return new Date(value);
 }
 
 
@@ -53,8 +53,9 @@ function parseDataFromIso8601(/* value */) {
  *    Date(2012,1,1)    => true
  *    Date(2015,1,1)    => false
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  const year = date.getFullYear();
+  return (year % 400 === 0 || year % 100 !== 0) && year % 4 === 0;
 }
 
 
@@ -73,8 +74,32 @@ function isLeapYear(/* date */) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+
+
+function timeSpanToString(startDate, endDate) {
+  function addZero(n) {
+    if (n < 10) {
+      return `0${n}`;
+    }
+    return n;
+  }
+  function formatMs(ms) {
+    if (ms < 100) {
+      if (ms < 10) {
+        return `00${ms}`;
+      }
+      return `0${ms}`;
+    }
+    return ms;
+  }
+  const diff = new Date(endDate - startDate);
+  const hours = diff.getUTCHours();
+  const minutes = diff.getMinutes();
+  const seconds = diff.getSeconds();
+  const miliseconds = diff.getMilliseconds();
+  return `${addZero(hours)}:${addZero(minutes)}:${addZero(
+    seconds,
+  )}.${formatMs(miliseconds)}`;
 }
 
 
@@ -94,10 +119,16 @@ function timeSpanToString(/* startDate, endDate */) {
  *    Date.UTC(2016,3,5,18, 0) => Math.PI
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
-function angleBetweenClockHands(/* date */) {
-  throw new Error('Not implemented');
+function angleBetweenClockHands(date) {
+  const utcDate = new Date(date);
+  const hours = utcDate.getUTCHours() >= 12
+    ? utcDate.getUTCHours() - 12
+    : utcDate.getUTCHours();
+  const minutes = utcDate.getUTCMinutes();
+  let clockAngle = 0.5 * (60 * hours + minutes) - 6 * minutes;
+  clockAngle = clockAngle > 180 ? (clockAngle -= 180) : clockAngle;
+  return Math.abs((clockAngle * Math.PI) / 180);
 }
-
 
 module.exports = {
   parseDataFromRfc2822,
